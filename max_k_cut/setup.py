f86519ae5bf7aee83e7bd22ef01bc4434714a1b3
fakhimi <fakhimi.ramin@gmail.com>
2023-01-25 19:50:30 -0500

    or_letters instances have been added

--- max_k_cut/setup.py
+++ max_k_cut/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='max_k_cut',
-      version='0.0.3',
+      version='0.0.4',
       description='Max k-cut problem',
       author='Ramin Fakhimi, Hamidreza Validi',
       author_email='fakhimi@lehigh.edu',
